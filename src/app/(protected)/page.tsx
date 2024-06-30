9913179a6489a0dc086fededc1bc5b09348449b5
Laurent Chau <mplaurent.chau@gmail.com>
2024-06-30 09:18:32 +0200

    ci: deploy app to render

--- src/app/(protected)/page.tsx
+++ src/app/(protected)/page.tsx
@@ -1,7 +1,7 @@
 export default function RootPage() {
   return (
     <main className="grid place-items-center h-full">
-      <h1 className="text-4xl">Hello, World!</h1>
+      <h1 className="text-4xl">Hello, Render!</h1>
     </main>
   );
 }
